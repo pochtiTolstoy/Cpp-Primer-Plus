b7c6e3d75df710fedb88341ffc710e0bc3cfea9d
tolstykhtimur <tolstihtimur@yandex.ru>
2023-12-06 07:24:50 +0300

    add t3

--- C4/t3.cpp
+++ C4/t3.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <cstring>
 
 int main() {
+	const int Size = 65;
+	const int N_symb = 3; // ',' ' ' '\0'
+
+	char first_name[Size];
+	std::cout << "Enter your first name: ";
+	std::cin.getline(first_name, Size);
+
+	char last_name[Size];
+	std::cout << "Enter your last name: ";
+	std::cin.getline(last_name, Size);
+
+	int len1 = strlen(first_name);
+	int len2 = strlen(last_name);
+	int new_size = len1 + len2 + N_symb;
+	char* concat_string = new char[new_size];
+	strcpy(concat_string, last_name);
+	strcat(concat_string, ", ");
+	strcat(concat_string, first_name);
+
+	std::cout << "Here's the information in a single string: " << concat_string << std::endl;
+
+	delete[] concat_string;
 
 	return 0;
 }
