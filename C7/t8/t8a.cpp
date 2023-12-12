183d5298e9b1cae215cf4499a1609c5adda45fa7
tolstykhtimur <tolstihtimur@yandex.ru>
2023-12-12 08:39:00 +0300

    Add t8a

--- C7/t8/t8a.cpp
+++ C7/t8/t8a.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
-#include <array>
+//#include <array>
 #include <string>
 
 const int Seasons = 4;
+/*
 const std::array<std::string, Seasons> Snames = 
 	{"Spring", "Summer", "Fall", "Winter"};
+*/
+const char* Snames[Seasons] = { "Spring", "Summer", "Fall", "Winter" };
 
+/*
 void fill(std::array<double, Seasons>* pa);
 void show(std::array<double, Seasons> da);
+*/
+void fill(double arr[]);
+void show(const double arr[]);
 
 int main() {
-	std::array<double, Seasons> expenses;
-	fill(&expenses);
+	//std::array<double, Seasons> expenses;
+	double expenses[Seasons];
+	fill(expenses);
 	show(expenses);
 	return 0;
 }
 
-void fill(std::array<double, Seasons>* pa) {
+void fill(double arr[]) {
 	for (int i = 0; i < Seasons; ++i) {
 		std::cout << "Enter " << Snames[i] << " expenses: ";
-		std::cin >> (*pa)[i];
+		std::cin >> arr[i];
 	}
 }
 
-void show(std::array<double, Seasons> da) {
+void show(const double arr[]) {
 	double total = 0.0;
 	std::cout << "\nEXPENSES\n";
 	for (int i = 0; i < Seasons; ++i) {
-		std::cout << Snames[i] << ": $" << da[i] << std::endl;
-		total += da[i];
+		std::cout << Snames[i] << ": $" << arr[i] << std::endl;
+		total += arr[i];
 	}
 	std::cout << "Total Expenses: $" << total << std::endl;
 }
