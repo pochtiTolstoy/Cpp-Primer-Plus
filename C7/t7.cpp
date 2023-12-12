d908b6b4e1d0098176bcfe9c910b15b98465a87c
tolstykhtimur <tolstihtimur@yandex.ru>
2023-12-12 08:16:01 +0300

    Add t7

--- C7/t7.cpp
+++ C7/t7.cpp
@@ -2,16 +2,17 @@
 
 const int Max = 5;
 
-int fill_array(double ar[], int limit);
-void show_array(const double ar[], int n);
-void revalue(double r, double ar[], int n);
+double* fill_array(double* begin, const double* const end);
+void show_array(const double* begin, const double* const end);
+void revalue(double r, double* begin, const double* const end);
 void clear_buffer(void);
 
 int main() {
 	double properties[Max];
-	int size = fill_array(properties, Max);
-	show_array(properties, size);
-	if (size > 0) {
+	double* begin = properties;
+	double* end = fill_array(begin, begin + Max);
+	show_array(begin, end);
+	if (begin != end) {
 		std::cout << "Enter revaluation factor: ";
 		double factor;
 		while (!(std::cin >> factor)) {
@@ -19,46 +20,44 @@ int main() {
 			clear_buffer();
 			std::cout << "Bad input; Please enter a number: ";
 		}
-		revalue(factor, properties, size);
-		show_array(properties, size);
+		revalue(factor, begin, end);
+		show_array(begin, end);
 	}
-
 	std::cout << "End.\n";
-
 	return 0;
 }
 
-void clear_buffer(void) {
-	while (std::cin.get() != '\n')
-		continue;
-}
-
-int fill_array(double ar[], int limit) {
+double* fill_array(double* begin, const double* const end) {
 	double temp;
-	int i;
-	for (i = 0; i < limit; ++i) {
-		std::cout << "Enter value #" << (i + 1) << ": ";
+	int id = 1;
+	while (begin < end) {
+		std::cout << "Enter value #" << id++ << ": ";
 		if (!(std::cin >> temp)) {
 			std::cout << "Bad input; input process terminated.\n";
 			break;
 		} else if (temp < 0)
 			break;
-		ar[i] = temp;
+		*begin++ = temp;
 	}
 	if (std::cin.fail())
 		std::cin.clear();
 	clear_buffer();
-	return i;
+	return begin;
 }
 
-void show_array(const double ar[], int n) {
-	for (int i = 0; i < n; ++i) {
-		std::cout << "Property #" << (i + 1) << ": $";
-		std::cout << ar[i] << std::endl;
-	}
+void revalue(double r, double* begin, const double* const end) {
+	while (begin < end)
+		*begin++ *= r;
 }
 
-void revalue(double r, double ar[], int n) {
-	for (int i = 0; i < n; ++i)
-		ar[i] *= r;
+void show_array(const double* begin, const double* const end) {
+	int id = 1;
+	while (begin < end)
+		std::cout << "Property #" << id++ << ": $"
+							<< *begin++ << std::endl;
+}
+
+void clear_buffer(void) {
+	while (std::cin.get() != '\n')
+		continue;
 }
